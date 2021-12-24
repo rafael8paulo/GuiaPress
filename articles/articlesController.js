e26904111644a7c71de40a6c567b9b5ca92746d9
Rafael Paulo <rafael8paulo@gmail.com>
2021-12-24 16:32:47 -0300

    Adicionando editor de texto e tiny mce

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category")
 
 router.get("/articles", (req, res) => {
   res.send("ROTA DE ARTIGOS");
 });
 
 router.get("/admin/articles/new", (req, res) => {
- res.send("ROTA PARA CIRAR UMA NOVO ARTIGO");
+  Category.findAll().then(categories => {
+    res.render("admin/articles/new", {categories: categories})
+  })
+ 
 });
 
 module.exports = router;
